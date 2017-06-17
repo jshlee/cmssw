9e27a60528cbaa7642c27af0a11478914fe0adf6
Cesare Calabria <cesare.calabria@ba.infn.it>
2017-06-17 14:28:40 +0200

    update IDs

--- SimMuon/MCTruth/plugins/MuonTrackProducer.cc
+++ SimMuon/MCTruth/plugins/MuonTrackProducer.cc
@@ -352,7 +352,7 @@ bool MuonTrackProducer::isLooseModExt(edm::Event& iEvent, reco::MuonCollection::
 //    if(fabs(eta) > 0 && fabs(eta) < 2.4) result = isPF && (isGLB || isTrk);
 //    else if(fabs(eta) > 2.4) result = isME0;
     
-    result = isPF && (isGLB || isTrk || isME0);
+    result = ( isPF && (isGLB || isTrk ) ) || isME0;
     
     return result;
 }
