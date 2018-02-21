c5599fae24bc850d4626c183e49331e9277463b6
Jason Lee <jason.lee@cern.ch>
2018-02-22 04:16:18 +0900

    fix CondFormats/GEMObjects/src/headers.h

--- CondFormats/GEMObjects/src/headers.h
+++ CondFormats/GEMObjects/src/headers.h
@@ -1,1 +1,6 @@
 #include "CondFormats/GEMObjects/interface/GEMELMap.h"
+
+
+#include "CondFormats/GEMObjects/interface/GEMDeadStrips.h"
+#include "CondFormats/GEMObjects/interface/GEMMaskedStrips.h"
+
