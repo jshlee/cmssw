b68d9657dd1e54aa49ad8a34cffedc6d00deda76
Jason Lee <jason.lee@cern.ch>
2018-02-28 16:11:35 +0100

    rm ME0DeadStrips/ME0MaskedStrips

---
+++ CondFormats/GEMObjects/src/T_EventSetup_GEMMaskedStrips.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/GEMObjects/interface/GEMMaskedStrips.h"
+#include "FWCore/Utilities/interface/typelookup.h"
+
+TYPELOOKUP_DATA_REG(GEMMaskedStrips);
--- CondFormats/GEMObjects/src/headers.h
+++ CondFormats/GEMObjects/src/headers.h
@@ -1,6 +1,4 @@
 #include "CondFormats/GEMObjects/interface/GEMELMap.h"
-
-
 #include "CondFormats/GEMObjects/interface/GEMDeadStrips.h"
 #include "CondFormats/GEMObjects/interface/GEMMaskedStrips.h"
 
